3d5b7508276d4e41ef8de4738e2f24a505bce03c
Son <sonteovs@gmail.com>
2024-03-22 15:44:11 +0700

    fix_someui

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -64,7 +64,7 @@ export default function Login() {
               Create Account
             </button>
           </div>
-          <Link to="/dashboard">
+          <Link to="/admin">
             <button
               type="submit"
               class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
---
+++ src/Pages/PageAdmin.js
@@ -0,0 +1,29 @@
+// import React from "react";
+// import { Route, Routes } from "react-router-dom";
+// import AppFooter from "../Components/AppFooter";
+// import NavHeader from "../Components/AppHeader/NavHeader";
+// import AccountManager from "./Customers/AccountManager";
+// import Deposit from "./Money/Deposit";
+// import Dashboard from "./Dashboard/Dashboard";
+// import SideMenu from "../Components/SideMenu/Menu";
+// // Import các component trang khác
+
+// export default function PageAdmin() {
+//   return (
+//     <div>
+//       <NavHeader />
+//       <div className="SideMenuAndPageContent flex flex-1">
+//         <SideMenu />
+//         <div className="bg-gray-200 w-full flex justify-center items-center">
+//           <Routes>
+//             <Route path="/admin" element={<Dashboard />} />
+//             <Route path="/admin/accounts" element={<AccountManager />} />
+//             <Route path="/admin/deposit" element={<Deposit />} />
+//             {/* Thêm các Route cho các trang khác */}
+//           </Routes>
+//         </div>
+//       </div>
+//       <AppFooter />
+//     </div>
+//   );
+// }
